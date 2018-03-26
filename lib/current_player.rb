bca03fc386a13fd660eace806cb8e1fb5ae6852e
Karen O <k.ortiz026@gmail.com>
2018-03-26 23:58:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,10 @@
 #Returns the the number of turns played
 def turn_count (board)
+  turn = 0
   board.each do |element|
-    number_x = 0
-    number_o = 0
-    if element == "X"
-      number_x += 1
-    elsif element == "O"
-      number_o += 1
+    if element == " X " || element == " O "
+      turn += 1
     end
   end
+  turn
 end
